f086db6ee3f7abdbb23a607dc1026480cdc707bc
SergioAlias <sergio08071998@gmail.com>
2024-04-18 18:54:47 +0200

    reads Excel file, modified gitignore

--- certificator.R
+++ certificator.R
@@ -2,9 +2,16 @@
 
 args = commandArgs(trailingOnly=TRUE)
 
-template <- args[1]
-names <- args[2]
-text_path <- args[3]
+library(openxlsx)
+
+template_path <- args[1]
+names_path <- args[2]
+xml_path <- args[3]
+
 tmp_folder <- tempdir()
 tmp_file <- tempfile()
+
+names <- openxlsx::read.xlsx(names_path)
+
+# 1-named certificate
 
